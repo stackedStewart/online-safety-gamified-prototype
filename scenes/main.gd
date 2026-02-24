568a741ab102c984e1928a63ab7b6d93e38c543e
stackedStewart <jclstewart87@gmail.com>
2026-02-24 16:46:58 +0000

    adding improved ui

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
-func _ready():
-	# Load the main menu when the game starts
+func _ready() -> void:
+	call_deferred("_go_to_menu")
+
+func _go_to_menu() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
